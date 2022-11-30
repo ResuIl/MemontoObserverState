ce426852555ecb9963528b6891941a13cd933baf
Rəsul <71267982+ResuIl@users.noreply.github.com>
2022-11-30 21:41:55 +0400

    Add project files.

---
+++ Memento/Program.cs
@@ -0,0 +1,46 @@
+﻿public class Life
+{
+    private string time;
+    public void Set(string time)
+    {
+        Console.WriteLine("Setting time to " + time);
+        this.time = time;
+    }
+    public Memento SaveToMemory()
+    {
+        Console.WriteLine("Saving time to memento");
+        return new Memento(time);
+    }
+    public void RestoreFromMemento(Memento memento)
+    {
+        time = memento.GetSavedTime();
+        Console.WriteLine("Time Restored from memento " + time);
+    }
+}
+
+public class Memento
+{
+    private readonly string time;
+    public Memento(string time)
+    {
+        this.time = time;
+    }
+    public string GetSavedTime() => time;
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Memento> SavedTimes = new List<Memento>();
+        Life life = new Life();
+        life.Set("12:00");
+        SavedTimes.Add(life.SaveToMemory());
+        life.Set("13:00");
+        SavedTimes.Add(life.SaveToMemory());
+        life.Set("14:00");
+        SavedTimes.Add(life.SaveToMemory());
+        life.Set("15:00");
+        life.RestoreFromMemento(SavedTimes[2]);
+    }
+}
---
+++ State/Program.cs
@@ -0,0 +1,40 @@
+﻿interface MobileAlertState
+{
+    void Alert(AlertStateContext ctx);
+}
+
+class AlertStateContext
+{
+    private MobileAlertState currentState;
+
+    public AlertStateContext()
+    {
+        currentState = new Vibration();
+    }
+
+    public void SetState(MobileAlertState state) => currentState = state;
+    public void Alert() => currentState.Alert(this);
+}
+
+class Vibration : MobileAlertState
+{
+    public void Alert(AlertStateContext ctx) => Console.WriteLine("Vibration...");
+}
+
+class Silent : MobileAlertState
+{
+    public void Alert(AlertStateContext ctx) => Console.WriteLine("Silent...");
+}
+
+class Program
+{
+    static void Main()
+    {
+        AlertStateContext alertStateContext= new AlertStateContext();
+        alertStateContext.Alert();
+        alertStateContext.Alert();
+        alertStateContext.Alert();
+        alertStateContext.SetState(new Silent());
+        alertStateContext.Alert();
+    }
+}
